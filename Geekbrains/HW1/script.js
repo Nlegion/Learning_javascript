71fcf17428144c32518f68443fca8933c479614e
Станислав Викторович <s.baykov@qtalents.co>
2020-12-08 22:02:54 +0300

    Revert "Revert "HW_1""
    This reverts commit d4d00eb4a536fbe01b1364b9fbbb095c090ceea1.

---
+++ Geekbrains/HW1/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Задание 1
+let TCelsius = 36.6;
+let TFahrenheit = (9 / 5) * TCelsius + 32
+
+console.log("Температура по Цельсию", TCelsius, " Температура по Фаренгейту", TFahrenheit);
+
+// Задание 2 - 3
+
+let name = 'Василий'
+let admin = name
+console.log(admin);
+
+// Задание 4
+console.log(1000 + "108");
+//  ответ:1000108
+
